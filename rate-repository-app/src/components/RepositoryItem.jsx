d6abb02ca35bc25a3fbc83c7ffbd0920473d0a06
César Henrique <cesaarhvb@gmail.com>
2023-11-25 20:26:21 -0300

    refactor:  RepositoryItem component

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,16 +1,47 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+import RepositoryItemHeader from "./RepositoryItemHeader";
+import RepositoryItemRating from "./RepositoryItemRating";
+
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.colors.backgroundSecondary,
+    padding: 15,
+  },
+});
 
 const RepositoryItem = ({ repository }) => {
+  const {
+    fullName,
+    description,
+    ownerAvatarUrl,
+    language,
+    forksCount,
+    stargazersCount,
+    ratingAverage,
+    reviewCount,
+  } = repository;
+
+  const repositoryItemHeaderProps = {
+    fullName,
+    description,
+    ownerAvatarUrl,
+    language,
+  };
+
+  const repositoryItemRatingProps = {
+    forksCount,
+    stargazersCount,
+    ratingAverage,
+    reviewCount,
+  };
+
   return (
-    <View>
-      <Text>Full name: {repository.fullName}</Text>
-      <Text>Description: {repository.description}</Text>
-      <Text>Language: {repository.language}</Text>
-      <Text>Stars: {repository.stargazersCount}</Text>
-      <Text>Forks: {repository.forksCount}</Text>
-      <Text>Reviews: {repository.reviewCount}</Text>
-      <Text>Rating: {repository.ratingAverage}</Text>
+    <View style={styles.container}>
+      <RepositoryItemHeader {...repositoryItemHeaderProps} />
+      <RepositoryItemRating {...repositoryItemRatingProps} />
     </View>
   );
 };
