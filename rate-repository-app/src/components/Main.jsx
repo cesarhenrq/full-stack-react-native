9cd37c591cb69a6517dd8a9a1383e43bd10df2d0
César Henrique <cesaarhvb@gmail.com>
2023-11-26 14:15:28 -0300

    feat: add SignIn component to Main.jsx

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -6,6 +6,7 @@ import { Route, Routes, Navigate } from "react-router-native";
 
 import RepositoryList from "./RepositoryList";
 import AppBar from "./AppBar";
+import SignIn from "./SignIn";
 
 import theme from "../theme";
 
@@ -24,6 +25,7 @@ const Main = () => {
       <AppBar />
       <Routes>
         <Route path='/' element={<RepositoryList />} />
+        <Route path='/signin' element={<SignIn />} />
         <Route path='*' element={<Navigate to='/' replace />} />
       </Routes>
     </View>
