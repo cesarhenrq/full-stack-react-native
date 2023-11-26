9edce35ef4f6a9d1487a3c564758c8964c0aaa60
César Henrique <cesaarhvb@gmail.com>
2023-11-26 13:56:14 -0300

    feat: add react-router-native to Main component

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -2,6 +2,8 @@ import Constants from "expo-constants";
 
 import { StyleSheet, View } from "react-native";
 
+import { Route, Routes, Navigate } from "react-router-native";
+
 import RepositoryList from "./RepositoryList";
 import AppBar from "./AppBar";
 
@@ -20,7 +22,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path='/' element={<RepositoryList />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
     </View>
   );
 };
