d591635f1be5b30d002b2fbae70c0c9328c899fb
Cééesar Henrique <cesaarhvb@gmail.com>
2023-11-28 23:09:25 -0300

    feat: add form validation to SignIn component

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,5 +1,13 @@
 import { Formik } from "formik";
+
 import SignInForm from "./SignInForm";
+
+import * as yup from "yup";
+
+const validationSchema = yup.object().shape({
+  username: yup.string().required("Username is required"),
+  password: yup.string().required("Password is required"),
+});
 
 const initialValues = {
   username: "",
@@ -12,7 +20,11 @@ const SignIn = () => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
       {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
     </Formik>
   );
