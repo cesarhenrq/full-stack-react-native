83ca8cfbbd0cc4f9c2acc9065a098367ad3858df
Cééesar Henrique <cesaarhvb@gmail.com>
2023-12-03 21:33:47 -0300

    feat: add loading indicator and error handling to RepositoryList component

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -1,4 +1,11 @@
-import { FlatList, View, StyleSheet } from "react-native";
+import React from "react";
+import {
+  FlatList,
+  View,
+  StyleSheet,
+  Text,
+  ActivityIndicator,
+} from "react-native";
 
 import { useQuery } from "@apollo/client";
 
@@ -6,20 +13,41 @@ import RepositoryItem from "./RepositoryItem";
 
 import { GET_REPOSITORIES } from "../graphql/queries";
 
+import theme from "../theme";
+
 const styles = StyleSheet.create({
   separator: {
     height: 10,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-  const { data } = useQuery(GET_REPOSITORIES, {
+  const { data, loading, error } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: "cache-and-network",
   });
 
-  console.log(data);
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size='large' color={theme.colors.appBarBackground} />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View>
+        <Text>Error: {error.message}</Text>
+      </View>
+    );
+  }
 
   const repositoryNodes = data.repositories
     ? data.repositories.edges.map((edge) => edge.node)
