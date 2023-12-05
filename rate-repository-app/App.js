3f128cf52c62f49ad0c474ef28bd78e4d1bcdd48
Cééesar Henrique <cesaarhvb@gmail.com>
2023-12-04 21:58:23 -0300

    feat: add AuthStorageContext to ApolloProvider

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -4,16 +4,23 @@ import { NativeRouter } from "react-router-native";
 import { ApolloProvider } from "@apollo/client";
 
 import Main from "./src/components/Main";
-import createApolloClient from "./src/utils/apolloClient";
 
-const apolloClient = createApolloClient();
+import createApolloClient from "./src/utils/apolloClient";
+import AuthStorage from "./src/utils/authStorage";
+
+import AuthStorageContext from "./src/contexts/AuthStorageContext";
+
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
   return (
     <>
       <NativeRouter>
         <ApolloProvider client={apolloClient}>
-          <Main />
+          <AuthStorageContext.Provider value={authStorage}>
+            <Main />
+          </AuthStorageContext.Provider>
         </ApolloProvider>
       </NativeRouter>
       <StatusBar style='auto' />
