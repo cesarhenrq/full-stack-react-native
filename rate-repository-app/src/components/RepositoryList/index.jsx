f3962fd33a2aa2093dfc18859e97d50ab2385050
Cééesar Henrique <cesaarhvb@gmail.com>
2023-12-05 22:41:09 -0300

    refactor: RepositoryList component

--- rate-repository-app/src/components/RepositoryList/index.jsx
+++ rate-repository-app/src/components/RepositoryList/index.jsx
@@ -1,23 +1,11 @@
-import { FlatList, View, StyleSheet } from "react-native";
-
 import Loading from "../Loading";
 import Error from "../Error";
-import RepositoryItem from "./RepositoryItem";
+import RepositoryListContainer from "./RepositoryListContainer";
 
 import useRepositories from "../../hooks/useRepositories";
 
-const styles = StyleSheet.create({
-  separator: {
-    height: 10,
-  },
-});
-
-const ItemSeparator = () => <View style={styles.separator} />;
-
 const RepositoryList = () => {
   const { repositories, loading, error } = useRepositories();
-
-  console.log("repositories", repositories);
 
   if (loading) {
     return <Loading />;
@@ -27,19 +15,7 @@ const RepositoryList = () => {
     return <Error error={error} />;
   }
 
-  const repositoryNodes = repositories
-    ? repositories.edges.map((edge) => edge.node)
-    : [];
-
-  return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item }) => (
-        <RepositoryItem key={item.id} repository={item} />
-      )}
-    />
-  );
+  return <RepositoryListContainer repositories={repositories} />;
 };
 
 export default RepositoryList;
