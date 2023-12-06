c7fcb8143a840f8b2b38be5912fed7f204008881
Cééesar Henrique <cesaarhvb@gmail.com>
2023-12-05 22:34:22 -0300

    refactor: useRepositories hook to use Apollo Client

--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -1,24 +1,21 @@
 import { useState, useEffect } from "react";
 
+import { useQuery } from "@apollo/client";
+
+import { GET_REPOSITORIES } from "../graphql/queries";
+
 const useRepositories = () => {
   const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
 
-  const fetchRepositories = async () => {
-    setLoading(true);
-
-    const response = await fetch("http://192.168.1.116:5000/api/repositories");
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
+  const { data, loading, error } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
 
   useEffect(() => {
-    fetchRepositories();
-  }, []);
+    setRepositories(data?.repositories);
+  }, [loading]);
 
-  return { repositories, loading, refetch: fetchRepositories };
+  return { repositories, loading, error };
 };
 
 export default useRepositories;
