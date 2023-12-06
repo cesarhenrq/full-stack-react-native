7497609be0a23800357978f916ba2f967dba894d
Cééesar Henrique <cesaarhvb@gmail.com>
2023-12-05 22:34:02 -0300

    refactor: remove unused imports and update repository data fetching

--- rate-repository-app/src/components/RepositoryList/index.jsx
+++ rate-repository-app/src/components/RepositoryList/index.jsx
@@ -1,12 +1,10 @@
-import { FlatList, View, StyleSheet, Text } from "react-native";
+import { FlatList, View, StyleSheet } from "react-native";
 
 import Loading from "../Loading";
-
-import { useQuery } from "@apollo/client";
-
+import Error from "../Error";
 import RepositoryItem from "./RepositoryItem";
 
-import { GET_REPOSITORIES } from "../../graphql/queries";
+import useRepositories from "../../hooks/useRepositories";
 
 const styles = StyleSheet.create({
   separator: {
@@ -17,24 +15,20 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-  const { data, loading, error } = useQuery(GET_REPOSITORIES, {
-    fetchPolicy: "cache-and-network",
-  });
+  const { repositories, loading, error } = useRepositories();
+
+  console.log("repositories", repositories);
 
   if (loading) {
     return <Loading />;
   }
 
   if (error) {
-    return (
-      <View>
-        <Text>Error: {error.message}</Text>
-      </View>
-    );
+    return <Error error={error} />;
   }
 
-  const repositoryNodes = data.repositories
-    ? data.repositories.edges.map((edge) => edge.node)
+  const repositoryNodes = repositories
+    ? repositories.edges.map((edge) => edge.node)
     : [];
 
   return (
